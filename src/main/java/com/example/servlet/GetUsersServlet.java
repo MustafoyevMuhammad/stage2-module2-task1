36769d353ec3ad4678369a133ba660f0747063df
Mustafoyev <mmustafoyev9@gmail.com>
2022-12-31 12:25:56 +0500

    commit message

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/users")
 public class GetUsersServlet extends HttpServlet {
 
     private final Warehouse warehouse = Warehouse.getInstance();
